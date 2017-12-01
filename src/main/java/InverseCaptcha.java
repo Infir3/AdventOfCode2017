0a332470673c0a368d1a0f4e514e7cfaf338fa08
Stefan Becking <stefanbecking@gmail.com>
2017-12-01 17:42:54 +0100

    solve circular captcha

--- src/main/java/InverseCaptcha.java
+++ src/main/java/InverseCaptcha.java
@@ -18,4 +18,37 @@ public class InverseCaptcha {
 
         return sum;
     }
+
+    public int solveCaptchaCircular(String input) {
+        int sum = 0;
+
+        int currentDigit;
+        int nextDigit;
+
+        int currentPos = 0;
+        int stepsForward = input.length() / 2;
+
+        char[] charArray = input.toCharArray();
+
+        nextDigit = getNextCircularNumber(charArray, 0, stepsForward);
+        for (char c : charArray) {
+            currentDigit = Character.getNumericValue(c);
+            if (currentDigit == nextDigit) {
+                sum += currentDigit;
+            }
+            nextDigit = getNextCircularNumber(charArray, currentPos + 1, stepsForward);
+            currentPos++;
+        }
+
+        return sum;
+    }
+
+    private int getNextCircularNumber(char[] charArray, int currentPosition, int stepsForward) {
+        int pos = currentPosition + stepsForward;
+        if (pos >= charArray.length) {
+            pos = pos % charArray.length;
+        }
+        return Character.getNumericValue(charArray[pos]);
+
+    }
 }
